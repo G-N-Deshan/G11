8b0286502e84e95e0aa5662eafc5754a2c2676b3
ndeshan <nipunadeshanganewaththa@gmail.com>
2026-02-01 17:04:39 +0530

    Develop the pages navigationbar and footer.

---
+++ php/db.php
@@ -0,0 +1,15 @@
+<?php
+
+    $host = "localhost";
+    $user = "root";
+    $password = "";
+    $dbname = "g11_vehical_system";
+
+    $conn = mysqli_connect($host, $user, $password, $dbname);
+
+    if(!$conn){
+        die("database connection failed.".mysqli_connect_error());
+    }
+
+
+?>
